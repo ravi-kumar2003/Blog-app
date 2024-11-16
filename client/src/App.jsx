de31ed520f037aaf2654bf0e580f203896f6431f
Ravi kumar <ravisah62020@gmail.com>
2024-11-16 15:51:40 +0530

    Header created

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+export default function App() {
+  return (
+    <BrowserRouter future={{ v7_relativeSplatPath: true , v7_startTransition: true }}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import React from "react";
+import { MdDarkMode } from "react-icons/md";
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link to="/" className="self-center whitespace-nowrap ">
+        <span className="p-2 bg-gradient-to-r from-indigo-500 via-purple-900 to-pink-500 rounded-md">
+          TeachDaddy
+        </span>
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={FaSearch}
+          className="hidden lg:inline"
+        ></TextInput>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <FaSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <MdDarkMode />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="tealToLime" className="p-1 rounded-3xl">
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
